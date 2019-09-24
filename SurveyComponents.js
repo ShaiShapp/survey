419c45c1177fa5aaf7ac8026efe8d10f9973b2e4
Shai Shapira <shai@handysoft.co.il>
2019-09-24 19:34:38 +0200

    Various refactoring and polishing.

--- SurveyComponents.js
+++ SurveyComponents.js
@@ -4,20 +4,20 @@ Vue.component('survey-enum-option', {
 })
 
 Vue.component('survey-question', {
-  props: ['title', 'def', 'type', 'options', 'required'],
+  props: ['title', 'def', 'type', 'options', 'required', 'idx'],
   data: function () {
             return {
-                valid: 1,
+                validvalue: 1,
                 errormessage: "",
-                value: ""
+                value: this.def
             }
         },
   methods: {validate: validateAnswer},
   template: '<div> \
-                 <p v-if="errormessage">{{ errormessage }}</p>\
-                 <div v-if="type === \'text\'"><label>{{ title }}:</label><input type="text" v-bind:value="def" v-on:change="validate"/></div> \
-                 <div v-if="type === \'number\'"><label>{{ title }}:</label><input type="number" v-bind:value="def" v-on:change="validate" /></div> \
-                 <div v-if="type === \'email\'"><label>{{ title }}:</label><input type="email" v-bind:value="def" v-on:change="validate" /></div> \
+                 <p v-if="errormessage">{{ errormessage }}</p> \
+                 <div v-if="type === \'text\'"><label>{{ title }}:</label><input type="text" v-bind:value="value" v-on:change="validate"/></div> \
+                 <div v-if="type === \'number\'"><label>{{ title }}:</label><input type="number" v-bind:value="value" v-on:change="validate" /></div> \
+                 <div v-if="type === \'email\'"><label>{{ title }}:</label><input type="email" v-bind:value="value" v-on:change="validate" /></div> \
                  <div v-if="type === \'enum\'"><label>{{ title }}:</label><survey-enum-option v-for="option, index in options" v-bind:caption="option" v-bind:key="index" /></div> \
              </div>'
 })
@@ -26,7 +26,7 @@ function validateAnswer(event)
 {
     let newValue = event.target.value;
     this.errormessage = "";
-    if (this.required && newValue === '') this.errormessage = "Please enter a value";
+    if (this.required && newValue === "") this.errormessage = "Please enter a value";
     else
     {
         switch (this.type)
@@ -37,10 +37,16 @@ function validateAnswer(event)
                                this.errormessage = "Please enter a valid number";
                            }
                            break;
-            case "email": break;
+            case "email": if (!/\S+@\S+\.\S+/.test(newValue)) // Email validation is tricky. We want to go for a relatively permissive one, to minimize false positives.
+                          {
+                            this.errormessage = "Please enter a valid email address";
+                          }
+                          break;
             case "enum": break;
         }
     }
-    this.valid = (this.errormessage === "");
-    this.$emit('input');
+    this.validvalue = (this.errormessage === "");
+    this.value = newValue;
+    //this.$emit('input', {answer: newValue, index: this.idx});
+    this.$emit('input', newValue, this.idx);
 }
